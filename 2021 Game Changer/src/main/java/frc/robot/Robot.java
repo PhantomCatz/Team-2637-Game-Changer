50428a90ff828bb1f4fd5b4a28484dace7c0333a
JeanKwon <34558849+JeanKwon@users.noreply.github.com>
2021-03-28 23:55:28 -0700

    Updated CSD auton code DL 3/26

--- 2021 Game Changer/src/main/java/frc/robot/Robot.java
+++ 2021 Game Changer/src/main/java/frc/robot/Robot.java
@@ -7,14 +7,31 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import java.util.ArrayList;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.Mechanisms.CatzShooter;
-import frc.Mechanisms.CatzElevator;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.SPI.Port;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import frc.DataLogger.CatzLog;
+import frc.DataLogger.DataCollection;
+
+import frc.Autonomous.CatzAutonomous;
+
+//import frc.Mechanisms.CatzShooter;
+//import frc.Mechanisms.CatzElevator;
+import frc.Mechanisms.CatzIntake;
+import frc.Mechanisms.CatzDriveTrain;
+
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -25,27 +42,42 @@ import frc.Mechanisms.CatzElevator;
  */
 public class Robot extends TimedRobot 
 {
+  //public static CatzShooter shooter;
+  //public static CatzElevator elevator;
+  public static CatzIntake intake;
+  public static CatzDriveTrain driveTrain;
 
+  public static CatzAutonomous auton;
+  public static Timer autonomousTimer;
 
-  public static CatzShooter shooter;
-  public static CatzElevator elevator;
+  public static DataCollection dataCollection;
+  public static CatzLog        catzLog;
+  public static Timer dataCollectionTimer;
+  public ArrayList<CatzLog> dataArrayList;
+
+  public static PowerDistributionPanel pdp;
+
+  public static AHRS navx;
   
+//-------------------------------------------Drive Controller------------------------------------------------------------- 
   public static XboxController xboxDrv;
   public static XboxController xboxAux;
 
   private final int XBOX_DRV_PORT = 0;
   private final int XBOX_AUX_PORT = 1;
-
-
-  public final double ELE_POWER = 0.5;
-  public final double ELE_FACTOR = 1.3;
-
- 
-
   private final int DPAD_UP = 0;
   private final int DPAD_DN = 180;
   private final int DPAD_LT = 270;
   private final int DPAD_RT = 90;
+
+//-------------------------------------------Elevator------------------------------------------------------------- 
+  public final double ELEV_POWER = 0.5;
+  public final double ELEV_FACTOR = 1.3;
+
+//-------------------------------------------Path Chooser------------------------------------------------------------- 
+  
+  
+  
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
@@ -57,18 +89,29 @@ public class Robot extends TimedRobot
     xboxDrv = new XboxController(XBOX_DRV_PORT);
     xboxAux = new XboxController(XBOX_AUX_PORT);
 
-    shooter = new CatzShooter();
-    elevator = new CatzElevator();
+    driveTrain = new CatzDriveTrain();
+    intake = new CatzIntake();
+    auton = new CatzAutonomous();
+    /*shooter = new CatzShooter();
+    elevator = new CatzElevator();*/
+    dataCollection = new DataCollection();
+
+    dataArrayList = new ArrayList<CatzLog>();
   
+    navx = new AHRS(Port.kMXP, (byte)200);
+    navx.reset();
     
-
-    xboxAux = new XboxController(1);
-    shooter = new CatzShooter();
-
-
-  elevator = new CatzElevator();
-  
+    pdp = new PowerDistributionPanel();
     
+    autonomousTimer     = new Timer();
+    dataCollectionTimer = new Timer();
+
+    dataCollection.dataCollectionInit(dataArrayList);
+    dataCollectionTimer.reset();
+    dataCollectionTimer.start();
+    dataCollection.setLogDataID(dataCollection.LOG_ID_DRV_STRAIGHT);
+    //dataCollection.setLogDataID(dataCollection.LOG_ID_DRV_TURN);
+    dataCollection.startDataCollection();
 
   }
 
@@ -84,8 +127,7 @@ public class Robot extends TimedRobot
 
   public void robotPeriodic() 
   {
-    SmartDashboard.putNumber("shaft velocity (RPM)", shooter.getFlywheelShaftVelocity());
-    SmartDashboard.putNumber("shooterState", CatzShooter.shooterState);
+    
   }
 
   /**
@@ -102,7 +144,9 @@ public class Robot extends TimedRobot
   @Override
   public void autonomousInit() 
   {
-
+    intake.deployIntake();
+    Timer.delay(0.5);
+    auton.driveStraight(120, 100);
   }
 
   /**
@@ -122,6 +166,7 @@ public class Robot extends TimedRobot
   @Override
   public void teleopInit() 
   {
+    driveTrain.instantiateDifferentialDrive();
   }
 
   /**
@@ -131,7 +176,40 @@ public class Robot extends TimedRobot
 
   public void teleopPeriodic() 
   {
-      //-----------------------shooter-----------------------
+    //-------------------------------------------Drivetrain------------------------------------------------------------- 
+    driveTrain.arcadeDrive(xboxDrv.getY(Hand.kLeft), xboxDrv.getX(Hand.kRight));
+  
+    if(xboxDrv.getBumper(Hand.kLeft))
+    {
+      driveTrain.shiftToHighGear();
+    }
+    else if(xboxDrv.getBumper(Hand.kRight))
+    {
+      driveTrain.shiftToLowGear();
+    }
+    //-------------------------------------------Intake------------------------------------------------------------- 
+    if(xboxDrv.getStickButtonPressed(Hand.kLeft))
+    {
+      intake.intakeDeployMC.set(0.25);
+    }
+    else if(xboxDrv.getStickButtonPressed(Hand.kRight))
+    {
+      intake.intakeDeployMC.set(-0.25);
+    }
+    
+    if(xboxDrv.getTriggerAxis(Hand.kLeft) > 0.2)
+    {
+      intake.intakeRollerIn();
+    }
+    else if(xboxDrv.getTriggerAxis(Hand.kRight) > 0.2)
+    {
+      intake.intakeRollerOut();
+    }
+    else
+    {
+      intake.intakeRollerOff();
+    }
+    /*//-----------------------shooter-----------------------
       if(xboxAux.getPOV() == DPAD_UP)
       {
         shooter.setTargetRPM(shooter.SHOOTER_TARGET_RPM_LO);
@@ -159,15 +237,15 @@ public class Robot extends TimedRobot
 
       if(xboxDrv.getAButton() == true)
       {
-        elevator.runElevatorA(ELE_POWER);
-        elevator.runElevatorB(ELE_POWER * ELE_FACTOR);
+        elevator.runElevatorA(ELE_POWER)V;
+        elevator.runElevatorB(ELE_POWER * ELEV_FACTOR);
       }
       else
       {
         elevator.runElevatorA(0);
         elevator.runElevatorB(0);
 
-      }
+      }*/
   }
 
   /**
@@ -176,6 +254,20 @@ public class Robot extends TimedRobot
   @Override
   public void disabledInit() 
   {
+    dataCollection.stopDataCollection();
+    Robot.driveTrain.drvTrainMtrCtrlLTFrnt.setNeutralMode(NeutralMode.Coast); 
+    Robot.driveTrain.drvTrainMtrCtrlLTBack.setNeutralMode(NeutralMode.Coast);
+    Robot.driveTrain.drvTrainMtrCtrlRTFrnt.setNeutralMode(NeutralMode.Coast);
+    Robot.driveTrain.drvTrainMtrCtrlRTBack.setNeutralMode(NeutralMode.Coast);
+
+    try 
+    {
+      dataCollection.exportData(dataArrayList);
+    } catch (Exception e) 
+    {
+      e.printStackTrace();
+    }
+
   }
 
   /**
